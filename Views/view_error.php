636c9f86822dc110a8ad1fdc5f4ea25a75fa599e
Abubakar Mzf <72223629+AbubakarMzf@users.noreply.github.com>
2021-04-14 15:01:03 +0200

    Update view_error.php

--- Views/view_error.php
+++ Views/view_error.php
@@ -1,4 +1,1 @@
-<?php require_once 'view_begin.php' ?>
-<?php require_once 'view_message.php'?>
-<a href="/?controller=login"> click here to log in </a>
-<?php require_once 'view_end.php'?>
+Code privé pour l'interface admin. 
