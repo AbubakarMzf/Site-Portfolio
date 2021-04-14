29b054d7d16c41b6541aa19ecdecfd4f1cf5b9d8
Abubakar Mzf <72223629+AbubakarMzf@users.noreply.github.com>
2021-04-14 15:00:22 +0200

    Update view_login.php

--- Views/view_login.php
+++ Views/view_login.php
@@ -1,8 +1,1 @@
-<!-- <p>FEATURES & TEST</p> -->
-
-
-<form action="/?controller=login&action=check" method="post">
-<p> <label>Login<input type="text" name="login"/> </label> </p>
-<p> <label>Password<input type="password" name="password"/> </label></p>
-<input type="submit" value="log in"/>
-</form>
+Formulaire de connexion pour l'interface admin. Code privé.  
